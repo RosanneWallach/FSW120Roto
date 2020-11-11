f9569a376b72f7c622e3ee192e8768cff891fe7b
Rosanne Wallach <66342005+RosanneWallach@users.noreply.github.com>
2020-11-11 01:26:23 -0500

    Add files via upload

---
+++ src/About/about.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './about.css'
+
+function About() {
+  return (
+    <div className="about-container">
+      <h1 className="about-title">Roto-Router</h1>
+      <h2 className="family-info"> Welcome to Doctor Drains, your number one source for all things plumbing. We're dedicated to giving you the very best of service, with a focus on dependability, customer service and honesty. </h2>
+      <h2 h2 className="family-info">We hope you are happy with our services, as much as we enjoy offering them to you. If you have any questions or comments, please don't hesitate to contact us.</h2>
+      <h2 h2 className="family-info">Sincerely, The Roto Family, Owners of Doctor Drains</h2>
+    </div>
+  );
+}
+
+export default About;
